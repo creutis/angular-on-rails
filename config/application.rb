c9132e4489bb4331e12622600f0c3d4b0e2a6187
Christian Reuterwall <christian.reuterwall@gmail.com>
2015-05-06 23:44:35 +0300

    added css js to compile

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module TodoMvc
     # config.i18n.default_locale = :de
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
-    #config.assets.precompile.shift
+    config.assets.precompile.shift
 
     # include Bower components in compiled assets
     config.assets.paths << Rails.root.join('app', 'assets')
@@ -33,6 +33,7 @@ module TodoMvc
         '.html', '.erb', '.haml',                 # Templates
         '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
         '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        '.css', 'js'                              # CSS AND JS  
       ]
     end)
   end
