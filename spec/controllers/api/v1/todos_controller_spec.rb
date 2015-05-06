414c83563ae4ac364f4ee01b9b1b3538f140b0c2
Christian Reuterwall <christian.reuterwall@gmail.com>
2015-05-06 10:40:15 +0300

    Added some commenst regarding CI and fixed some tests.

--- spec/controllers/api/v1/todos_controller_spec.rb
+++ spec/controllers/api/v1/todos_controller_spec.rb
@@ -10,11 +10,9 @@ describe Api::V1::TodosController do
 
 		it "returns all todos" do
 			FactoryGirl.create_list :todo, 5
-			#FactoryGirl.create_list(:todo, 5)
 			get :index, format: :json
 			parsed_response = JSON.parse(response.body)
 			expect(parsed_response.length).to eq 5
-			#xpect(parsed_response['todo'].length).to eq(5)
 		end
 	end
 
