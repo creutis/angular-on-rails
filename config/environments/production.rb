3424c57ee1a1c04c3faface78c78bac270f53c3b
Christian Reuterwall <christian.reuterwall@gmail.com>
2015-05-07 07:59:28 +0300

    Added some more settings for precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,9 @@ TodoMvc::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
+  config.serve_static_assets = true
+  config.assets.compress = true
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
