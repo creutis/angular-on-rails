b4d6a7ea0318335f0974d2026f1871a352e9f70b
Christian Reuterwall <christian.reuterwall@gmail.com>
2015-05-07 08:15:02 +0300

    removed uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ TodoMvc::Application.configure do
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Generate digests for assets URLs.
